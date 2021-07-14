eae4ac6c21b94dad285e3cf02911a6ca1dd50499
ysz <1582945670@qq.com>
2021-07-14 18:24:03 +0800

    初始项目

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// 全局样式
+import 'styles/index.scss'
+
+// element3
+import element3 from 'plugins/element3'
+
+// router
+import router from '@/router'
+
+// store
+import store from '@/store'
+
+// i18n
+import { createI18n } from "vue-i18n"
+import messages from "@intlify/vite-plugin-vue-i18n/messages"
+console.log(messages);
+const i18n = createI18n({
+  legacy: false,
+  locale: "en",
+  messages,
+})
+
+createApp(App).use(router).use(store).use(element3).use(i18n).mount('#app')
